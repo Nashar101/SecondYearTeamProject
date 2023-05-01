4de7035a44042a31c545939d70cdfee5d3438ba5
Nashar101 <nashar101@gmail.com>
2023-05-01 19:50:33 +0100

    final commit frfr

--- src/main/webapp/app/history/history.component.ts
+++ src/main/webapp/app/history/history.component.ts
@@ -10,11 +10,11 @@ import { AccountService } from '../core/auth/account.service';
 import { HttpClient } from '@angular/common/http';
 export class list {
   ID!: number;
-  subject!: string;
-  subjectScore!: number;
-  subjectTarget!: string;
-  upcomingTest!: string;
-  upcomingTestTarget!: string;
+  subjectScore: number[] = [];
+  subjectTarget: number[] = [];
+  upcomingTest: string[] = [];
+  upcomingTestTarget: number[] = [];
+  subject: string[] = [];
 }
 @Component({
   selector: 'jhi-history',
@@ -23,81 +23,104 @@ export class list {
 })
 export class HistoryComponent implements OnInit {
   historyItems?: IHistory[];
-  newList: list[] = [];
+  //@ts-ignore
+  newList: list;
 
   init(): void {
     this.http.get<any>('/api/account').subscribe(account => {
       const userID = account.id;
+      console.log('this is admin id' + userID);
       this.historyListService.query().subscribe(response => {
         this.historyItems = response.body || [];
         this.historyItems = this.historyItems.filter(list => list.user?.id === userID);
-
+        let historyList = new list();
+        console.log('TESTING' + this.historyItems[0].subject);
         for (let i = 0; i < this.historyItems.length; i++) {
-          let historyList = new list();
-          historyList.ID = this.historyItems[i].id;
           //@ts-ignore
-          historyList.subject = this.historyItems[i].subject;
+          historyList.subject[i] = this.historyItems[i].subject;
           //@ts-ignore
-          historyList.subjectScore = this.historyItems[i].subjectScore;
+          historyList.subjectScore[i] = this.historyItems[i].subjectScore;
           //@ts-ignore
-          historyList.subjectTarget = this.historyItems[i].subjectTarget;
+          historyList.subjectTarget[i] = this.historyItems[i].subjectTarget;
           //@ts-ignore
-          historyList.upcomingTest = this.historyItems[i].upcomingTest;
+          historyList.upcomingTest[i] = this.historyItems[i].upcomingTest;
           //@ts-ignore
-          historyList.upcomingTestTarget = this.historyItems[i].upcomingTestTarget;
-          this.newList.push(historyList);
-          historyList = new list();
+          historyList.upcomingTestTarget[i] = this.historyItems[i].upcomingTestTarget;
         }
+        this.newList = historyList;
+        let labels: string[] = ['Maths Test 1', 'English Test 1', 'Biology Test 1', 'Maths Test 2', 'Maths Test 3'];
+        let itemData: number[] = [80, 70, 85, 90, 87];
+        var myChart = new Chart('myChart', {
+          type: 'line',
+          data: {
+            labels: this.newList.subject,
+            datasets: [
+              {
+                label: 'Test Grades',
+                data: this.newList.subjectScore,
+                borderWidth: 4,
+                borderColor: '#C20505',
+              },
+            ],
+          },
+          options: {
+            scales: {
+              y: {
+                beginAtZero: true,
+                border: {
+                  color: '#F5F5F5',
+                },
+                grid: {
+                  color: '#FAF8F8',
+                },
+                ticks: {
+                  color: '#A8A8A8',
+                },
+              },
+              x: {
+                border: {
+                  color: '#FAF8F8',
+                },
+                grid: {
+                  display: false,
+                },
+                ticks: {
+                  color: '#A8A8A8',
+                },
+              },
+            },
+          },
+        });
       });
     });
   }
-
   constructor(private accountService: AccountService, private http: HttpClient, protected historyListService: HistoryService) {}
 
+  /**add(): void{
+    this.accountService.identity().subscribe(account => {
+      if (account) {
+        console.log(account.login);
+        this.http.get<any>('/api/account').subscribe(response => {
+          const userId = response.id;
+          //@ts-ignore
+          const newItem: NewHistory = {
+            subject: this.newList[0].subject,
+            subjectScore:  this.newList[0].subjectScore,
+            subjectTarget: this.newList[0].subjectTarget,
+            upcomingTest: this.newList[0].upcomingTest,
+            upcomingTestTarget : this.newList[0].upcomingTestTarget,
+            user: { id: userId, login: account.login },
+          };
+          this.historyListService.create(newItem).subscribe();
+          this.init();
+        });
+      }
+    });
+
+  }**/
   ngOnInit(): void {
     let labels: string[] = ['Maths Test 1', 'English Test 1', 'Biology Test 1', 'Maths Test 2', 'Maths Test 3'];
     let itemData: number[] = [80, 70, 85, 90, 87];
-
-    var myChart = new Chart('myChart', {
-      type: 'line',
-      data: {
-        labels: labels,
-        datasets: [
-          {
-            label: 'Test Grades',
-            data: itemData,
-            borderWidth: 4,
-            borderColor: '#C20505',
-          },
-        ],
-      },
-      options: {
-        scales: {
-          y: {
-            beginAtZero: true,
-            border: {
-              color: '#F5F5F5',
-            },
-            grid: {
-              color: '#FAF8F8',
-            },
-            ticks: {
-              color: '#A8A8A8',
-            },
-          },
-          x: {
-            border: {
-              color: '#FAF8F8',
-            },
-            grid: {
-              display: false,
-            },
-            ticks: {
-              color: '#A8A8A8',
-            },
-          },
-        },
-      },
-    });
+    this.init();
   }
 }
